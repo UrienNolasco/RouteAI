4491af1bda16a66152073bf21c4122dfe564c005
UrienNolasco <urien.dragon@gmail.com>
2024-08-18 19:14:22 -0300

    Finalizada integração ao gpt

--- app/index.tsx
+++ app/index.tsx
@@ -1,59 +1,109 @@
-import {useState} from 'react';
-import { MaterialIcons } from "@expo/vector-icons";
-import Slider from "@react-native-community/slider";
-import { Text, View, StyleSheet, StatusBar, TextInput, Platform, Pressable, ScrollView, ActivityIndicator } from "react-native";
+import { useState } from 'react'
+import {
+  StyleSheet, Text, View, StatusBar, TextInput, Platform, Pressable, ScrollView,
+  ActivityIndicator, Alert, Keyboard
+} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
+import Slider from '@react-native-community/slider'
 
-const StatusBarHeight = StatusBar.currentHeight;
+const statusBarHeight = StatusBar.currentHeight
+const KEY_GPT = 'SUA_API_KEY';
 
-export default function Index() {
+export default function App() {
 
   const [city, setCity] = useState("");
-  const [days, setDays] = useState(1);
+  const [days, setDays] = useState(3);
   const [loading, setLoading] = useState(false);
   const [travel, setTravel] = useState("")
 
+  async function handleGenerate() {
+    if (city === "") {
+      Alert.alert("Atenção", "Preencha o nome da cidade!")
+      return;
+    }
+
+    setTravel("")
+    setLoading(true);
+    Keyboard.dismiss();
+
+    const prompt = `Crie um roteiro para uma viagem de exatos ${days.toFixed(0)} dias na cidade de ${city}, busque por lugares turisticos, lugares mais visitados, seja preciso nos dias de estadia fornecidos e limite o roteiro apenas na cidade fornecida. Forneça apenas em tópicos com nome do local onde ir em cada dia.`
+
+    fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${KEY_GPT}`
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: 'user',
+            content: prompt
+          }
+        ],
+        temperature: 0.20,
+        max_tokens: 500,
+        top_p: 1,
+      })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data.choices[0].message.content);
+        setTravel(data.choices[0].message.content)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+
+  }
+
   return (
     <View style={styles.container}>
-      <StatusBar barStyle="dark-content" translucent={true} backgroundColor="#F1F1F1"/>
-      <Text style={styles.heading}>Route AI</Text>
+      <StatusBar barStyle="dark-content" translucent={true} backgroundColor="#F1F1F1" />
+      <Text style={styles.heading}>Roteiros fácil</Text>
 
       <View style={styles.form}>
-        <Text style={styles.label}>Cidade de Destino</Text>
-        <TextInput style={styles.input} placeholder="Ex: Porto Alegre, RS"
+        <Text style={styles.label}>Cidade destino</Text>
+        <TextInput
+          placeholder="Ex: Campo Grande, MS"
+          style={styles.input}
           value={city}
           onChangeText={(text) => setCity(text)}
         />
-        <Text style={styles.label}>Tempo de estadia: <Text style={styles.days}> {days.toFixed()} </Text> dias</Text>
 
+        <Text style={styles.label}>Tempo de estadia: <Text style={styles.days}> {days.toFixed(0)} </Text> dias</Text>
         <Slider
           minimumValue={1}
-          maximumValue={31}
-          minimumTrackTintColor="#F1A10A"
+          maximumValue={7}
+          minimumTrackTintColor="#009688"
           maximumTrackTintColor="#000000"
-          thumbTintColor="#F1A10A"
           value={days}
           onValueChange={(value) => setDays(value)}
         />
       </View>
 
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={handleGenerate}>
         <Text style={styles.buttonText}>Gerar roteiro</Text>
-        <MaterialIcons name="travel-explore" size={24} color="#F1A10A "/>
+        <MaterialIcons name="travel-explore" size={24} color="#FFF" />
       </Pressable>
 
-      <ScrollView contentContainerStyle={{ paddingBottom:16 }} style={styles.containerScroll} showsVerticalScrollIndicator={false}>
-        {loading &&(
+      <ScrollView contentContainerStyle={{ paddingBottom: 24, marginTop: 4, }} style={styles.containerScroll} showsVerticalScrollIndicator={false} >
+        {loading && (
           <View style={styles.content}>
-          <Text style={styles.title}>Carregando roteiro...</Text>
-          <ActivityIndicator size="large" color="#F1A10A"/>
-        </View>             
+            <Text style={styles.title}>Carregando roteiro...</Text>
+            <ActivityIndicator color="#000" size="large" />
+          </View>
         )}
 
         {travel && (
           <View style={styles.content}>
-          <Text style={styles.title}>Roteiro da viagem 👇</Text>
-          <Text>{travel}</Text>
-        </View>        
+            <Text style={styles.title}>Rotreiro da viagem 👇</Text>
+            <Text style={{ lineHeight: 24, }}>{travel}</Text>
+          </View>
         )}
       </ScrollView>
 
@@ -62,18 +112,18 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor : '#F1F1F1',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     paddingTop: 20,
   },
-  heading:{
+  heading: {
     fontSize: 32,
     fontWeight: 'bold',
-    paddingTop: Platform.OS === 'android' ? StatusBarHeight : 54,
+    paddingTop: Platform.OS === 'android' ? statusBarHeight : 54
   },
-  form:{
+  form: {
     backgroundColor: '#FFF',
     width: '90%',
     borderRadius: 8,
@@ -81,52 +131,52 @@ const styles = StyleSheet.create({
     marginTop: 16,
     marginBottom: 8,
   },
-  label:{
+  label: {
     fontWeight: 'bold',
     fontSize: 18,
     marginBottom: 8,
   },
-  input:{
+  input: {
     borderWidth: 1,
     borderRadius: 4,
-    borderColor: '#94A3B8',
+    borderColor: '#94a3b8',
     padding: 8,
     fontSize: 16,
-    marginBottom: 16 ,
+    marginBottom: 16,
   },
-  days:{
-    color: '#F1A10A',
-    fontWeight: 'bold',
+  days: {
+    backgroundColor: '#F1f1f1'
   },
-  button:{
-    margin: 16,
-    backgroundColor: '#F1A10A',
+  button: {
+    backgroundColor: '#FF5656',
     width: '90%',
+    borderRadius: 8,
     flexDirection: 'row',
     padding: 14,
     justifyContent: 'center',
-    alignItems:'center',
-    borderRadius: 8,  
-    gap: 10,
-    marginBottom: 24
+    alignItems: 'center',
+    gap: 8,
   },
-  buttonText:{
+  buttonText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    color: '#FFF',
+    fontWeight: 'bold'
   },
-  content:{
+  content: {
     backgroundColor: '#FFF',
     padding: 16,
-    borderRadius: 8,
     width: '100%',
+    marginTop: 16,
+    borderRadius: 8,
   },
-  title:{
+  title: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 14
   },
-  containerScroll:{
+  containerScroll: {
     width: '90%',
+    marginTop: 8,
   }
 });
